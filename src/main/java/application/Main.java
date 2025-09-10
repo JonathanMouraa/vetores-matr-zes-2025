f451f6ae00bd1150c4b0a3cc0f1c75c3fc4fa9e2
JonathanMouraa <270908@unifio.edu.br>
2025-09-10 01:33:36 +0000

    Aula 09/09

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,7 +2,13 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        //Declaração de Vetores
+        int[] numeros = new int[5];
+
+
+        //Declaração de Matrizes 
+        int[][] matiz = new int[5][3];
+
     }
 }
 

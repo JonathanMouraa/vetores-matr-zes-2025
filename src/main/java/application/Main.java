bb76a2f39820313f9ace34553cdc83d7f9f52b40
JonathanMouraa <270908@unifio.edu.br>
2025-09-17 01:33:32 +0000

    Aula

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import application.lista.ListaSimples;
+
 public class Main {
     public static void main(String[] args) {
         //Declaração de Vetores
@@ -9,6 +14,18 @@ public class Main {
         //Declaração de Matrizes 
         int[][] matiz = new int[5][3];
 
-    }
+
+        //declaração de Lista Simples
+        ListaSimples lista = new ListaSimples();
+        lista.adicionar("Primeiro");
+        lista.adicionar("Segundo");
+        lista.adicionar("terceiro");
+        lista.remover(1);
+
+        List<String> lista2 = new ArrayList<String>();
+        lista2.add("Teste");
+
+        System.out.println(lista);
+        }
 }
 

92d24c1f13d38946e3afc26a96177294e6becf35
JonathanMouraa <270908@unifio.edu.br>
2025-09-24 01:20:04 +0000

    aula

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import application.lista.ListaSimples;
+import application.pilha.PilhaSimples;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,6 +27,17 @@ public class Main {
         lista2.add("Teste");
 
         System.out.println(lista);
+
+        PilhaSimples pilha = new PilhaSimples(3);
+        pilha.empilhar("Primeiro Elemento");
+        pilha.empilhar("Segundo Elemento");
+
+        System.out.println(pilha.desempilhar());
+        pilha.empilhar("Terceiro Elemento");
+        System.out.println(pilha.desempilhar());
+        System.out.println(pilha.desempilhar());
+        System.out.println(pilha.desempilhar());
+        
         }
 }
 
---
+++ src/main/java/application/pilha/PilhaSimples.java
@@ -0,0 +1,44 @@
+package application.pilha;
+
+public class PilhaSimples {
+    private String [] itens;
+    private int topo;
+    private int tamanho;
+
+    public PilhaSimples(int tamanho){
+        this.tamanho = tamanho;
+        this.itens = new String[tamanho];
+        this.topo = -1;
+    }
+    public void empilhar(String valor){
+        if(topo == (tamanho - 1)) {
+            System.out.println("Pilha Cheia!!!");
+        }else {
+            // topo++;
+            //itens[topo] = valor;
+
+            itens[++topo] = valor;
+        }
+    }
+
+    public String desempilhar(){
+        String resultado = " ";
+        if (topo == -1){
+            System.out.println("Pilha Vazial!!!");
+        }else {
+            resultado = itens[topo--];
+        }
+        return resultado;
+
+    }
+    @Override
+
+    public String toString(){
+        String resultado = "";
+
+        for (String i : itens) {
+            resultado += i +" ";
+        }
+        return resultado;
+    }
+}
